5ce9d2456aaff1c50957a951d935436dad65c057
Mainguy, Michael <michael.mainguy@accenture.com>
2019-10-07 15:32:07 -0500

    updated nodejs

--- bin/acn-nodejs.sh
+++ bin/acn-nodejs.sh
@@ -5,6 +5,7 @@ function __acn_nodejs {
    fi
    case "$COMMAND" in
     install)
+      mkdir -p "$ACN_PLATFORM_DIR/nodejs"
       curl https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.0/install.sh -o "$ACN_PLATFORM_DIR"/nodejs/install.sh
       chmod 777 "$ACN_PLATFORM_DIR"/nodejs/install.sh
       (cd "$ACN_PLATFORM_DIR"/nodejs; ./install.sh) || return 1
