2e721586730326fc1e8c312fbc011bf2ee4ac9de
Mainguy, Michael <michael.mainguy@accenture.com>
2019-10-07 16:31:35 -0500

    added more instructions

--- acn-setup.sh
+++ acn-setup.sh
@@ -6,6 +6,14 @@ if [ -z "$ACN_HOME_DIR" ]; then
 	git clone https://innersource.accenture.com/scm/dqt/start-here.git $ACN_HOME_DIR
 fi
 (cd $ACN_HOME_DIR; git pull)
+echo
+echo "either exit this window and relauch\n or type:"
+echo
+echo "[[ -s \"$HOME/.acn/acn-init.sh\" ]] && source \"$HOME/.acn/acn-init.sh\""
+echo
+echo "once you've done either of these, type:"
+echo
+echo "acn"
 
 
 
