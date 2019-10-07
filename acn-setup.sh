51a013b3becd24c02f1d51c50393e0269b7a0d4a
Mainguy, Michael <michael.mainguy@accenture.com>
2019-10-07 13:50:41 -0500

    fixed env variables

--- acn-setup.sh
+++ acn-setup.sh
@@ -2,11 +2,12 @@
 
 if [ -z "$ACN_HOME_DIR" ]; then
 	export ACN_HOME_DIR="$HOME/.acn"
+	echo "[[ -s \"$HOME/.acn/acn-init.sh\" ]] && source \"$HOME/.acn/acn-init.sh\"" >> "$HOME/.bash_profile"
 	git clone https://innersource.accenture.com/scm/dqt/start-here.git $ACN_HOME_DIR
 fi
 (cd $ACN_HOME_DIR; git pull)
 
-echo "[[ -s \"$HOME/.acn/acn-init.sh\" ]] && source \"$HOME/.acn/acn-init.sh\"" >> "$HOME/.bash_profile"
+
 source "$ACN_HOME_DIR/acn-init.sh"
 
 
