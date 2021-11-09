2236c4011687646f426384876bf133283629d0f7
mike mainguy <mike.mainguy@gmail.com>
2021-11-09 08:32:29 -0600

    Added Heroku

--- bin/exp-heroku.sh
+++ bin/exp-heroku.sh
@@ -1,15 +1,21 @@
 function __exp_heroku {
+   if ! npm_loc="$(type -p "npm")" || [[ -z $npm_loc ]]; then
+     echo "please install nodejs first:"
+     echo "exp nodejs install"
+     return 1
+   fi
+
    if [[ -z "$COMMAND" ]]; then
      echo "Available commands:"
      echo " install - install latest Heroku cli"
    fi
+
+
    case "$COMMAND" in
-    install)
-      mkdir -p "$EXP_PLATFORM_DIR/heroku"
-      curl https://cli-assets.heroku.com/install.sh -o "$EXP_PLATFORM_DIR"/heroku/install.sh
-      chmod 777 "$EXP_PLATFORM_DIR"/heroku/install.sh
-      (cd "$EXP_PLATFORM_DIR"/heroku; ./install.sh) || return 1
-      echo "nvm install...exit shell and relaunch, then run 'heroku'";;
+            install)
+              npm install -g heroku
+              echo "nvm install...exit shell and relaunch, then run 'heroku'";;
    esac
+
   return 0
 }
